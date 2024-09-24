4b059ea2ddfbde86a486b8e9c0ba26e4c945191a
pata98 <yoopata@postech.ac.kr>
2024-09-25 03:26:52 +0900

    not use mcp2515,. use python-can

--- src/mg4005e_can.py
+++ src/mg4005e_can.py
@@ -1,14 +1,9 @@
-# MG4005E-i10 v3 CAN Driver
-# Author        : Jiho Ryoo
-# Affiliation   : POSTECH, CoCEL
-# Contact       : yoopata@postech.ac.kr
-# Date          : 2024.09.10
-
-from package.MCP2515 import MCP2515
+import can
 import time
 
 class MotorDriver:
-    """Class to handle CAN communication for the MG4005E-i10 motor driver."""
+    """Class to handle CAN communication for the MG4005E-i10 motor driver using python-can."""
+    
     # Command Constants
     IDENTIFIER_BASE         = 0x140
     DATA_LENGTH             = 8
@@ -24,6 +19,8 @@ class MotorDriver:
     CMD_INCREMENT_ANGLE_2   = 0xA8
     CMD_ANGLE_CONTROL_SL_1  = 0xA5
     CMD_ANGLE_CONTROL_SL_2  = 0xA6
+    CMD_ZEROING             = 0x19
+    CMD_READ_MOTOR_STATE    = 0x9C
 
     COMMAND_BYTE            = 0 
     RESPOND_TEMP            = 1
@@ -33,41 +30,25 @@ class MotorDriver:
     RESPOND_SPEED_H         = 5
     RESPOND_ENCODER_L       = 6
     RESPOND_ENCODER_H       = 7
+    RESPOND_SPEED_COEF      = 720/16383
+    RESPOND_ENCODER_COEF    = 90/16383
 
     CW                      = 0x00
     CCW                     = 0x01
 
-    def __init__(self):
+    ## Motor ID
+    ID_MOTOR_PAN            = 0x01
+    ID_MOTOR_TILT           = 0x02
+    
+
+    def __init__(self, bustype='slcan', channel='/dev/ttyACM0', bitrate=1000000):
         """
-        Initialize the MotorDriver with a CAN interface using MCP2515.
-        
-        Args:
-            channel (str): The CAN channel (default: 'can0').
+        Initialize the MotorDriver with a CAN interface using python-can.
         """
-        self.can = MCP2515()
-        self.can.Init()
+        # Initialize python-can bus
+        self.bus = can.interface.Bus(bustype=bustype, channel=channel, bitrate=bitrate)
 
     #### CAN Message ###############################################################
-    ## Tx/Rx
-    def _send_message(self, message_id, data):
-        """Send a CAN message to the bus using MCP2515."""
-        message = self.can.Message(arbitration_id=message_id, data=data, is_extended_id=False)
-        try:
-            self.can.Send(message)
-        except can.CanError as e:
-            raise RuntimeError("Failed to send message: {}".format(e))
-
-    def _receive_message(self):
-        """Receive a CAN message with a timeout."""
-        try:
-            message = self.bus.recv(1.0)
-            if message is not None:
-                return message
-        except self.can.CanError as e:
-            raise RuntimeError("Error occurred while receiving message: {}".format(e))
-        return None
-    
-    ## Processing
     def _build_message(self, command, payload=None):
         """Build a CAN message with the command byte and optional payload."""
         if payload is None:
@@ -78,51 +59,108 @@ class MotorDriver:
         """Convert a control value into its byte representation."""
         return [(value >> (8 * i)) & 0xFF for i in range(byte_length)]
 
-    #### Response Parsing ##########################################################
+    def _send_motor_command(self, motor_id, command, payload=None):
+        """Send a motor command using the python-can library."""
+        message_id = self.IDENTIFIER_BASE + motor_id
+        data = self._build_message(command, payload)
+
+        msg = can.Message(arbitration_id=message_id, data=data, is_extended_id=False)
+
+        try:
+            self.bus.send(msg)
+            # print(f"Message sent: ID={hex(message_id)}, Data={data}")
+        except can.CanError:
+            print("Message NOT sent")
+        
+        # Try to receive the response
+        try:
+            response = self.bus.recv(timeout=1.0)  # Wait for 1 second for a response
+            if response is not None:
+                return self._parse_response(response)
+            else:
+                print("No response received")
+                return {}
+        except Exception as e:
+            raise RuntimeError(f"Error occurred while receiving message: {e}")
+
     def _parse_response(self, message):
         """Parse response message to extract motor state values."""
         command = message.data[self.COMMAND_BYTE]
         temp = message.data[self.RESPOND_TEMP]
         current = (message.data[self.RESPOND_CURRENT_H] << 8) | message.data[self.RESPOND_CURRENT_L]
         encoder = (message.data[self.RESPOND_ENCODER_H] << 8) | message.data[self.RESPOND_ENCODER_L]
-        control = (message.data[self.RESPOND_SPEED_H] << 8) | message.data[self.RESPOND_SPEED_L]
+        speed = (message.data[self.RESPOND_SPEED_H] << 8) | message.data[self.RESPOND_SPEED_L]
+
+        # Sign
+        speed = speed - 0x10000 if speed & 0x8000 else speed
+        encoder = encoder * self.RESPOND_ENCODER_COEF if encoder * self.RESPOND_ENCODER_COEF <= 180 else (encoder * self.RESPOND_ENCODER_COEF - 360)
+
         return {
             'command': command,
             'temperature': temp,
             'torque_current': current,
             'angle': encoder,
-            'speed': control
+            'speed': speed * self.RESPOND_SPEED_COEF
         }
 
-    #### Generalized Motor Command Function ########################################
-    def _send_motor_command(self, motor_id, command, payload=None):
-        """General method to send a motor command and receive its response."""
-        message_id = self.IDENTIFIER_BASE + motor_id
-        data = self._build_message(command, payload)
-        self._send_message(message_id, data)
-        response = self._receive_message()
-        return self._parse_response(response) if response else {}
 
-    #### Motor Power Control #######################################################
+    #### Motor Power Control / Setting #############################################
     def motor_on(self, motor_id):
-        return self._send_motor_command(motor_id, self.CMD_MOTOR_ON)
+        response = self._send_motor_command(motor_id, self.CMD_MOTOR_ON)
+        motor_name='PAN' if motor_id == self.ID_MOTOR_PAN else 'TILT'
+        if response.get('command') == self.CMD_MOTOR_ON:
+            print(f'{motor_name} POWER ON') 
+        else:
+            print(f'{motor_name} ON FAILURE!')
+        return response
 
     def motor_off(self, motor_id):
-        return self._send_motor_command(motor_id, self.CMD_MOTOR_OFF)
+        response = self._send_motor_command(motor_id, self.CMD_MOTOR_OFF)
+        motor_name='PAN' if motor_id == self.ID_MOTOR_PAN else 'TILT'
+        if response.get('command') == self.CMD_MOTOR_OFF:
+            print(f'{motor_name} POWER OFF') 
+        else:
+            print(f'{motor_name} OFF FAILURE!')
+        return response
 
     def motor_stop(self, motor_id):
-        return self._send_motor_command(motor_id, self.CMD_MOTOR_STOP)
+        response = self._send_motor_command(motor_id, self.CMD_MOTOR_STOP)
+        motor_name='PAN' if motor_id == self.ID_MOTOR_PAN else 'TILT'
+        if response.get('command') == self.CMD_MOTOR_STOP:
+            print(f'{motor_name} STOP') 
+        else:
+            print(f'{motor_name} STOP FAILURE!')
+        return response
+    
+    def motor_zeroing(self, motor_id):
+        """Zeroing Motor. It writes to ROM, which will affect the chip life. DO NOT USE IT FREQUENTLY!!"""
+        """  Also, note that motor should be powered off and on again to finish the zeroing procedure  """
+        response = self._send_motor_command(motor_id, self.CMD_ZEROING)
+        motor_name='PAN' if motor_id == self.ID_MOTOR_PAN else 'TILT'
+        if response.get('command') == self.CMD_ZEROING:
+            print(f'{motor_name} ZEROING SUCCESS') 
+        else:
+            print(f'{motor_name} ZEROING FAILURE')
+        return response
+    
 
     #### Motor Movement Control ####################################################
     def torque_control(self, motor_id, torque_value):
+        """Still Need Dev."""
         torque_bytes = self._control_value_bytes(torque_value)
         return self._send_motor_command(motor_id, self.CMD_TORQUE_CONTROL, [0x00, 0x00, 0x00] + torque_bytes)
 
     def speed_control(self, motor_id, speed_value):
-        speed_bytes = self._control_value_bytes(speed_value, byte_length=4)
+        speed_bytes = self._control_value_bytes(speed_value * 100, byte_length=4)   # Conver -> 1 input step = 0.01 dps/LSB
         return self._send_motor_command(motor_id, self.CMD_SPEED_CONTROL, [0x00, 0x00, 0x00] + speed_bytes)
 
-    #### Angle Control #############################################################
+
+    #### Monitor ###################################################################
+    def read_motor_state(self, motor_id):
+        return self._send_motor_command(motor_id, self.CMD_READ_MOTOR_STATE)
+        
+
+    ## Angle Control ###############################################################
     def angle_control_1(self, motor_id, angle_value):
         angle_bytes = self._control_value_bytes(angle_value, byte_length=4)
         return self._send_motor_command(motor_id, self.CMD_ANGLE_CONTROL_ML_1, [0x00, 0x00, 0x00] + angle_bytes)
@@ -132,7 +170,7 @@ class MotorDriver:
         max_speed_bytes = self._control_value_bytes(max_speed, byte_length=2, signed=False)
         return self._send_motor_command(motor_id, self.CMD_ANGLE_CONTROL_ML_2, [0x00] + max_speed_bytes + angle_bytes)
 
-    #### Incremental Angle Control #################################################
+    ## Incremental Angle Control ###################################################
     def increment_angle_control_1(self, motor_id, angle_increment):
         angle_increment_bytes = self._control_value_bytes(angle_increment, byte_length=4)
         return self._send_motor_command(motor_id, self.CMD_INCREMENT_ANGLE_1, [0x00, 0x00, 0x00] + angle_increment_bytes)
@@ -142,31 +180,50 @@ class MotorDriver:
         max_speed_bytes = self._control_value_bytes(max_speed, byte_length=2, signed=False)
         return self._send_motor_command(motor_id, self.CMD_INCREMENT_ANGLE_2, [0x00] + max_speed_bytes + angle_increment_bytes)
 
-    #### Single Loop Angle Control #################################################
+    ## Single Loop Angle Control ###################################################
     def single_loop_angle_control_1(self, motor_id, spin_direction, angle_value):
+        """Control angle"""
         angle_bytes = self._control_value_bytes(angle_value * 100, byte_length=4)
         return self._send_motor_command(motor_id, self.CMD_ANGLE_CONTROL_SL_1, [spin_direction, 0x00, 0x00] + angle_bytes)
 
     def single_loop_angle_control_2(self, motor_id, spin_direction, angle_value, max_speed):
-        angle_bytes = self._control_value_bytes(angle_value * 100, byte_length=4)
-        max_speed_bytes = self._control_value_bytes(max_speed, byte_length=2, signed=False)
+        """
+        Control angle
+
+        args: 
+        """
+        angle_value = angle_value if angle_value > 0 else (360 + angle_value)
+        angle_bytes = self._control_value_bytes(angle_value * 1000, byte_length=4)
+        max_speed_bytes = self._control_value_bytes(max_speed * 25, byte_length=2, signed=False)
         return self._send_motor_command(motor_id, self.CMD_ANGLE_CONTROL_SL_2, [spin_direction] + max_speed_bytes + angle_bytes)
 
 
 if __name__ == "__main__":
-    print "CAN Communication tester"
     # Instantiate the driver
     driver = MotorDriver()
 
-    # Example motor ID
-    motor_id = 1
-
     # Test sequence with returned status
-    print "Connection Test: Check CAN communication is well established."
-    motor_status = driver.motor_on(motor_id)
-    if motor_status['command'] == driver.CMD_MOTOR_ON:
-        print 'PASS'  
-    else:
-        print "FAIL!" 
-        exit(0)
-    time.sleep(1)
+    motor_status = driver.motor_on(driver.ID_MOTOR_PAN)
+    motor_status = driver.motor_on(driver.ID_MOTOR_TILT)
+
+    print("=============== Move Motor ===============")
+    print("Move PAN Motor to 10")
+    target_angle = 20
+    motor_status = driver.single_loop_angle_control_2(driver.ID_MOTOR_TILT, driver.CW, target_angle, 20)
+    while abs(motor_status.get('angle') - target_angle) > 0.5:
+        motor_status = driver.read_motor_state(driver.ID_MOTOR_TILT)
+        print("angle: ", motor_status.get('angle'))
+        print("speed: ", motor_status.get('speed'))
+    
+    # print("================ Monitor ================")
+    # try:
+    #     while True:
+    #         motor_status = driver.read_motor_state(driver.ID_MOTOR_TILT)
+    #         print("angle: ", motor_status.get('angle'))
+    #         print("speed: ", motor_status.get('speed'))
+    # except KeyboardInterrupt:
+    #     print("Exit")
+
+
+    motor_status = driver.motor_off(driver.ID_MOTOR_PAN)
+    motor_status = driver.motor_off(driver.ID_MOTOR_TILT)
