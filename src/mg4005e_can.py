7120fc6815b1bf619239342fc4318c25b028cd8a
pata98 <yoopata@postech.ac.kr>
2024-09-26 21:08:34 +0900

    fix bug, add pid tunning

--- src/mg4005e_can.py
+++ src/mg4005e_can.py
@@ -21,6 +21,9 @@ class MotorDriver:
     CMD_ANGLE_CONTROL_SL_2  = 0xA6
     CMD_ZEROING             = 0x19
     CMD_READ_MOTOR_STATE    = 0x9C
+    CMD_READ_PID            = 0x30
+    CMD_SET_PID_RAM         = 0x31
+    CMD_SET_PID_ROM         = 0x32
 
     COMMAND_BYTE            = 0 
     RESPOND_TEMP            = 1
@@ -57,7 +60,7 @@ class MotorDriver:
 
     def _control_value_bytes(self, value, byte_length=2, signed=True):
         """Convert a control value into its byte representation."""
-        return [(value >> (8 * i)) & 0xFF for i in range(byte_length)]
+        return [(int(value) >> (8 * i)) & 0xFF for i in range(byte_length)]
 
     def _send_motor_command(self, motor_id, command, payload=None):
         """Send a motor command using the python-can library."""
@@ -96,6 +99,7 @@ class MotorDriver:
         encoder = encoder * self.RESPOND_ENCODER_COEF if encoder * self.RESPOND_ENCODER_COEF <= 180 else (encoder * self.RESPOND_ENCODER_COEF - 360)
 
         return {
+            'raw': message.data,
             'command': command,
             'temperature': temp,
             'torque_current': current,
@@ -158,8 +162,31 @@ class MotorDriver:
     #### Monitor ###################################################################
     def read_motor_state(self, motor_id):
         return self._send_motor_command(motor_id, self.CMD_READ_MOTOR_STATE)
-        
+    
+    #### PID Tunning ###############################################################
+    def read_PID_gain(self, motor_id):
+        respond = self._send_motor_command(motor_id, self.CMD_READ_PID)
+        print(respond)
+        message = respond.get('raw')
 
+        return {
+            'position_p':   message[2],
+            'position_i':   message[3],
+            'speed_p':      message[4],
+            'speed_i':      message[5],
+            'torque_p':     message[6],
+            'torque_i':     message[7]
+        }
+    
+    def set_PID_gain_ram(self, motor_id, position_p, position_i, speed_p, speed_i, torque_p, torque_i):
+        pid_gains = [position_p, position_i, speed_p, speed_i, torque_p, torque_i]
+        return self._send_motor_command(motor_id, self.CMD_SET_PID_RAM, [0x00] + pid_gains)
+    
+    def set_PID_gain_rom(self, motor_id, position_p, position_i, speed_p, speed_i, torque_p, torque_i):
+        pid_gains = [position_p, position_i, speed_p, speed_i, torque_p, torque_i]
+        return self._send_motor_command(motor_id, self.CMD_SET_PID_ROM, [0x00] + pid_gains)
+
+        
     ## Angle Control ###############################################################
     def angle_control_1(self, motor_id, angle_value):
         angle_bytes = self._control_value_bytes(angle_value, byte_length=4)
@@ -190,12 +217,41 @@ class MotorDriver:
         """
         Control angle
 
-        args: 
+        Args:
+            motor_id        (int)   : The ID of the motor to control (e.g., pan or tilt motor).
+            spin_direction  (int)   : The direction of rotation (0x00 for clockwise, 0x01 for counterclockwise).
+            angle_value     (float) : The target angle to move the motor to, in degrees.
+            max_speed       (float) : The maximum speed for the motor movement in degrees per second.
+
+        Returns:
+            dict: A dictionary containing the motor response data, including the updated angle and speed.
         """
-        angle_value = angle_value if angle_value > 0 else (360 + angle_value)
+        angle_value = angle_value if angle_value >= 0 else (360 + angle_value)
         angle_bytes = self._control_value_bytes(angle_value * 1000, byte_length=4)
         max_speed_bytes = self._control_value_bytes(max_speed * 25, byte_length=2, signed=False)
         return self._send_motor_command(motor_id, self.CMD_ANGLE_CONTROL_SL_2, [spin_direction] + max_speed_bytes + angle_bytes)
+    
+
+    ## Easy-To-Use Angle Control ###################################################
+    def motor_move(self, motor_id, angle, max_speed, stay=False):
+        response = self.read_motor_state(motor_id)
+        if response.get('angle') - angle > 0.0001:
+            motor_direction = self.CCW
+        elif response.get('angle') - angle < 0.0001:
+            motor_direction = self.CW
+        else:
+            return response
+
+        response = self.single_loop_angle_control_2(motor_id, motor_direction, angle, max_speed)
+        if stay:
+            while abs(response.get('angle') - angle) > 0.05:
+                response = self.read_motor_state(motor_id)
+                # print("angle: ", response.get('angle'))
+                # print("speed: ", response.get('speed'))
+                # time.sleep(0.1)
+
+        return response
+
 
 
 if __name__ == "__main__":
@@ -208,13 +264,20 @@ if __name__ == "__main__":
 
     print("=============== Move Motor ===============")
     print("Move PAN Motor to 10")
-    target_angle = 20
-    motor_status = driver.single_loop_angle_control_2(driver.ID_MOTOR_TILT, driver.CW, target_angle, 20)
-    while abs(motor_status.get('angle') - target_angle) > 0.5:
-        motor_status = driver.read_motor_state(driver.ID_MOTOR_TILT)
-        print("angle: ", motor_status.get('angle'))
-        print("speed: ", motor_status.get('speed'))
+    target_angle = 0
+    motor_status = driver.motor_move(driver.ID_MOTOR_PAN, target_angle, 20, stay=True)
     
+    # print("================== PID ===================")
+    # print('Set PID I to 120')
+    # driver.set_PID_gain_ram(driver.ID_MOTOR_PAN, 100,120,40,14,50,50)
+    # pid_read = driver.read_PID_gain(driver.ID_MOTOR_PAN)
+    # print('Position P: ', pid_read.get('position_p'))
+    # print('Position I: ', pid_read.get('position_i'))
+    # print('Speed P: ', pid_read.get('speed_p'))
+    # print('Speed I: ', pid_read.get('speed_i'))
+    # print('torque P: ', pid_read.get('torque_p'))
+    # print('torque I: ', pid_read.get('torque_i'))
+
     # print("================ Monitor ================")
     # try:
     #     while True:
@@ -223,7 +286,6 @@ if __name__ == "__main__":
     #         print("speed: ", motor_status.get('speed'))
     # except KeyboardInterrupt:
     #     print("Exit")
-
 
     motor_status = driver.motor_off(driver.ID_MOTOR_PAN)
     motor_status = driver.motor_off(driver.ID_MOTOR_TILT)
